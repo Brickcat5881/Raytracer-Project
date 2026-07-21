d2e538188bbf577c42b81aa73d6eeeaae1eb3fff
Brickcat5881 <oliverhvnt@gmail.com>
2026-07-21 21:51:11 +0100

    Added the Sphere

--- main.cpp
+++ main.cpp
@@ -4,10 +4,32 @@
 
 #include <iostream>
 
+
+
+double hit_sphere(const point3& center, double radius, const ray& r) {
+        auto a = r.direction().length_squared();
+        auto h = dot(r.direction(), oc);
+        auto c = oc.length_squared() - radius*radius;
+        auto discriminant = h*h - a*c;
+        
+        if (discriminant < 0) {
+            return -1.0;
+        } else {
+            return (h - std::sqrt(discriminant)) / a;
+        }
+        
+    }
+
 color ray_color(const ray& r) {
+    auto t= hit_sphere(point3(0,0,-1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
+        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+    }
+    
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
-    return (1.0-a)*color(1.0,0.3,1.0) + a*color(0.9,0.7,0.2);
+    return (1.0-a)*color(1.0,1.0,1.0) + a*color(0.5,0.7,1.0);
 }
 
 int main() {
@@ -55,5 +77,6 @@ int main() {
             write_color(std::cout, pixel_color);
         }
     }
+    
     std::clog << "zrDone.       \n";
 }
