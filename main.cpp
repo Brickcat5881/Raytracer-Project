14fb370e4086b82b8ef7bc402ee8ef55c877704f
Brickcat5881 <oliverhvnt@gmail.com>
2026-08-16 22:39:46 +0100

    Started implementing JSON for managing scenes

--- main.cpp
+++ main.cpp
@@ -8,9 +8,41 @@
 #include "model_loader.h"
 #include "translate.h"
 
+#include <map>
+#include <fstream>
+#include "json.hpp"
+
+using json = nlohmann::json;
+
 
 int main() {
+    //Opening the JSON file for the scene
+    std::ifstream scene_file("Scenes/ringScene.json");
+    if (!scene_file.is_open()) {
+        std::cerr << "Failed to open the scene";
+        return 1;
+    }
+
+    //Parsing the JSON and turning it into the data variable
+    json data = json::parse(scene_file);
+    
     hittable_list world;
+
+    //Defining the dictionary "map" as std<KEY_TYPE, VALUE_TYPE>
+    //Essentially just defining the types from the json
+    std::map<std::string, shared_ptr<material>> materials; 
+
+    for (auto& [name, mat_data] : data["materials"].items()) {
+        std::string type = mat_data["material"];
+
+        if (type == "metal") {
+            color albedo(mat_data["albedo"][0], mat_data["albedo"][1], mat_data["albedo"][2]);
+            materials[name] = make_shared<metal>(albedo);
+        }
+    }
+
+
+
 
     //auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
     //world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
@@ -20,7 +52,7 @@ int main() {
 
     auto torus = make_shared<model>("OBJs/simpleRing.obj", triangle_material);
     world.add(make_shared<translate>(torus,vec3(0,1,0)));
-    
+
     camera cam;
 
     cam.aspect_ratio      = 16.0 / 9.0;
@@ -31,10 +63,10 @@ int main() {
     cam.vfov     = 12;
     cam.lookfrom = point3(13,2,3);
     cam.lookat   = point3(0,0,0);
-    cam.vup      = vec3(0,1,0);
+    cam.vup      = vec3(0,1,0); //Controls Roll, Input direction of vertically upwards for scene
 
-    cam.defocus_angle = 0.3;
-    cam.focus_dist    = (cam.lookfrom - cam.lookat).length();
+    cam.defocus_angle = 0.3; //Essetially aperture
+    cam.focus_dist    = (cam.lookfrom - cam.lookat).length(); //Focus Distance, currently set at being the lookat point
 
     cam.render(world);
 }
